7b3db6af50e89544ffa9174b50042be6411c7b0d
Alvaro <165071441+Allvarogou@users.noreply.github.com>
2024-09-27 10:51:49 -0300

    Atividade 3 e 4 da primeira lista

---
+++ Atividade3/src/num1.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class num1 {
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite um numero de 1 a 10: ");
+		int numero = leitor.nextInt();
+		String nomearquivo = "tabuada_" + numero + ".txt";
+
+		try (BufferedWriter escrita = new BufferedWriter(new FileWriter(nomearquivo))) {
+			for (int i = 1; i <= 10; i++) {
+				escrita.write(numero + "x" + i + "=" + (numero * i));
+				escrita.newLine();
+			}
+			System.out.println("Arquivo salvo em" + nomearquivo);
+		} catch (IOException e) {
+			System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+		} finally {
+			leitor.close();
+		}
+
+	}
+
+}
---
+++ Atividade3/src/num2.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class num2 {
+	public static void main(String[] args) throws IOException {
+
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite um numero de 1 a 10: ");
+		int numero = leitor.nextInt();
+		String nomearquivo = "tabuada_" + numero + ".txt";
+
+		File fr = new File(nomearquivo);
+		if (!fr.exists()) {
+			System.out.println("Arquivo não existe.");
+		} else {
+			try {
+				@SuppressWarnings("resource")
+				BufferedReader br = new BufferedReader(new FileReader(nomearquivo));
+				System.out.println("Lendo arquivo...");
+				String linha = "";
+				while ((linha = br.readLine()) != null) {
+					System.out.println(linha);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
